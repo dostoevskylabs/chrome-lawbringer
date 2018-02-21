d9d3974c5991b4f33f4f467970cdf1e847d1e5d1
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2018-02-20 22:20:42 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,25 @@
 chrome.processes.onUpdated.addListener(function(processes) {
 	for ( tabId in processes ) {
 		switch ( processes[tabId].id ) {
+			// ignore special tabs
 			case 0:
 			case 2:
 			case 42:
-			break;			
+			break;
+			
+			// otherwise watch the tab's usage
 			default:
+				// if the tab's cpu usage exceeds 200.0%
 				if ( processes[tabId].cpu > 200.0 ) {
+					// template for data to log to the extensions console
 					let template =  `
 					Terminated '${tabId}' due to exceeded resource usage.
 					Title: ${processes[tabId].tasks[0].title}
 					CPU: ${processes[tabId].cpu}
 					`;
+					
+					// terminate the process
+					// and log the data stored in the template above
 					chrome.processes.terminate(processes[tabId].id, function( didTerminate ) {
 						if ( didTerminate ) {
 							console.log(template);
