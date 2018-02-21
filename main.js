ecfe333dbc442f8bab28c407d25e4026bb8d6416
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2018-02-20 22:23:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+/*
+ * eventually I intended to take the scripts and run them against
+ * a service that determines if they are malicious
+ * and if so terminates that script and blocks it from running
+ */
+
 console.log("Chrome Lawbringer extension loaded.");
+// An array to store links to external js
+// and another to store the contents of inline scripts
 let externalScripts = [];
 let inlineScripts = [];
 
+// loop through all scripts in the html and store them in their appropriate arrays
 for ( let i = 0; i < ( document.scripts.length - 1 ); i++ ) {
 	if ( document.scripts[i].getAttribute("src") !== null ) {
 		externalScripts.push(document.scripts[i].getAttribute("src") );
@@ -10,12 +19,15 @@ for ( let i = 0; i < ( document.scripts.length - 1 ); i++ ) {
 	}
 }
 
+// print out the external scripts
 console.log("External Scripts");
 console.log("---------------");
 for ( script of externalScripts ) {
 	console.log(script);
 }
 console.log("---------------");
+
+// print out the inline scripts
 console.log("Inline Scripts");
 console.log("---------------");
 for ( script of inlineScripts ) {
